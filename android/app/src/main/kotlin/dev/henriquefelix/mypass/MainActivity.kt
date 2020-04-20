a1ed321cf76dbd42dc34c00a295e332ac06fce8a
HenriqueFelixDev <henriquefelixdev@gmail.com>
2020-04-20 19:12:03 -0300

    Substituição do channel do flutter do beta para o master

--- android/app/src/main/kotlin/dev/henriquefelix/mypass/MainActivity.kt
+++ android/app/src/main/kotlin/dev/henriquefelix/mypass/MainActivity.kt
@@ -1,6 +1,12 @@
 package dev.henriquefelix.mypass
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
