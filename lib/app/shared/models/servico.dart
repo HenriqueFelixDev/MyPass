53e9e364ceea406a24c55962de46abe6d2db94e9
HenriqueFelixDev <henriquefelixdev@gmail.com>
2020-04-19 21:33:38 -0300

    Criação do model servico

---
+++ lib/app/shared/models/servico.dart
@@ -0,0 +1,11 @@
+class Servico {
+  int id;
+  String identificador;
+  String servico;
+  String usuario;
+  String email;
+  String senha;
+  String informacoes;
+
+  Servico(this.id, this.identificador, this.servico, this.usuario, this.email, this.senha, this.informacoes);
+}
